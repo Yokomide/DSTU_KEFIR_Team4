cf947df8a8b7d86da761ce05e4d63ad898a50d42
Maksim Demian <thebox070@gmail.com>
2021-03-23 18:28:43 +0300

    аааа

---
+++ Fantasy/Assets/FantasyCastleDemo/Scripts/cameraDemoCastle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraDemoCastle : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+        float X = Input.GetAxis("Mouse X");
+
+        transform.eulerAngles += Vector3.up * X * 100f * Time.deltaTime;
+
+        transform.position += transform.TransformDirection(Vector3.ClampMagnitude(new Vector3(H, 0, V), 1f)) * speed * Time.deltaTime;
+    }
+}
