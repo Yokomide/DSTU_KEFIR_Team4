3b339a33cbdebca916acfa68e95039bdede60a71
fuett966 <76002087+fuett966@users.noreply.github.com>
2021-04-02 20:43:01 +0300

    Bag system(not working)
    add: class for bag that have information about items in bag

--- Fantasy/Assets/Inventory/Scripts/Bag.cs
+++ Fantasy/Assets/Inventory/Scripts/Bag.cs
@@ -5,14 +5,25 @@ using UnityEngine.EventSystems;
 
 public class Bag : MonoBehaviour, IPointerClickHandler
 {
+    [System.Serializable]
+    public class Bag_Items
+    {
+        public string name;
+        public GameObject item;
+        public int count_Items;
+        public int drop_Rarity;
+        public string pathItem;
+        public string pathIcon;
+    }
+
+    public List<Bag_Items> bag_Items = new List<Bag_Items>();
+
     [HideInInspector]
     public int index;
 
     GameObject playerObject;
     GameObject inventoryObject;
     Inventory inventory;
-
-    public string pathItems;
 
     void Start()
     {
@@ -23,21 +34,19 @@ public class Bag : MonoBehaviour, IPointerClickHandler
     {
         if (eventData.button == PointerEventData.InputButton.Left) // нопки мыши : Left Right Middle
         {
-            if (inventory.items[index].id == 5)
+            GameObject droppedObject = Instantiate(Resources.Load<GameObject>(bag_Items[Random.Range(0,3)].pathItem));
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+            droppedObject.transform.position = playerObject.transform.position + new Vector3(-1, +1, 0);
+            if (inventory.items[index].countItem > 1)
             {
-                GameObject droppedObject = Instantiate(Resources.Load<GameObject>(inventory.items[index].pathPrefab));
-                playerObject = GameObject.FindGameObjectWithTag("Player");
-                droppedObject.transform.position = playerObject.transform.position + new Vector3(-1, +1, 0);
-                if (inventory.items[index].countItem > 1)
-                {
-                    inventory.items[index].countItem--;
-                }
-                else
-                {
-                    inventory.items[index] = new Items();
-                }
-                inventory.DisplayItem();
+                inventory.items[index].countItem--;
             }
+            else
+            {
+                inventory.items[index] = new Items();
+            }
+            inventory.DisplayItem();
         }
     }
 }
+
